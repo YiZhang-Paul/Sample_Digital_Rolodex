39ba9876b8d9aa040503704ab08e8104035020a6
YI ZHANG <scaccterran@gmail.com>
2018-01-04 01:54:28 -0500

    client name validation

--- DigitalRolodex/DigitalRolodexClassLibrary/RolodexValidator.cs
+++ DigitalRolodex/DigitalRolodexClassLibrary/RolodexValidator.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace DigitalRolodexClassLibrary {
     public class RolodexValidator : IRolodexValidator {
 
+        private bool ContainsDigit(string name) {
+
+            return Regex.IsMatch(name, @"\d");
+        }
+
+        private bool IsValidLength(string name) {
+
+            return name.Length >= 2 && name.Length <= 25;
+        }
+
         public bool IsValidName(string name) {
 
-            return true;
+            name = name.Trim().ToLower();
+
+            return IsValidLength(name) && !ContainsDigit(name);
         }
     }
 }
--- DigitalRolodex/DigitalRolodexTests/RolodexValidatorTest.cs
+++ DigitalRolodex/DigitalRolodexTests/RolodexValidatorTest.cs
@@ -18,7 +18,27 @@ namespace DigitalRolodexTests {
         public void NameIsEmpty() {
 
             Assert.IsFalse(validator.IsValidName(""));
-            Assert.IsFalse(validator.IsValidName("".PadLeft(5, '\0')));
+            Assert.IsFalse(validator.IsValidName("".PadLeft(5, " "[0])));
+        }
+
+        [TestMethod]
+        public void NameWithDigit() {
+
+            Assert.IsFalse(validator.IsValidName("John Do5"));
+            Assert.IsFalse(validator.IsValidName("5 52"));
+            Assert.IsFalse(validator.IsValidName("Jane1e"));
+        }
+
+        [TestMethod]
+        public void NameWithOneCharacter() {
+
+            Assert.IsFalse(validator.IsValidName("J"));
+        }
+
+        [TestMethod]
+        public void NameWithMoreThanTwentyFiveCharacter() {
+
+            Assert.IsFalse(validator.IsValidName("J".PadLeft(26, 'J')));
         }
     }
 }
