d15220e97f0f466e85060cd6fbdf4559273c4376
YI ZHANG <scaccterran@gmail.com>
2018-01-04 01:39:55 -0500

    RolodexValidator Class

---
+++ DigitalRolodex/DigitalRolodexClassLibrary/RolodexValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalRolodexClassLibrary {
+    public class RolodexValidator : IRolodexValidator {
+
+        public bool IsValidName(string name) {
+
+            return true;
+        }
+    }
+}
---
+++ DigitalRolodex/DigitalRolodexTests/RolodexValidatorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DigitalRolodexClassLibrary;
+
+namespace DigitalRolodexTests {
+    [TestClass]
+    public class RolodexValidatorTest {
+
+        RolodexValidator validator;
+
+        [TestInitialize]
+        public void Setup() {
+
+            validator = new RolodexValidator();
+        }
+
+        [TestMethod]
+        public void NameIsEmpty() {
+
+            Assert.IsFalse(validator.IsValidName(""));
+            Assert.IsFalse(validator.IsValidName("".PadLeft(5, '\0')));
+        }
+    }
+}
