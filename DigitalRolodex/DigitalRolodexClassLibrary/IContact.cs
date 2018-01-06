ba15b99b7447259af6b9001a863fbb4854f60915
YI ZHANG <scaccterran@gmail.com>
2018-01-06 03:08:59 -0500

    contact class

---
+++ DigitalRolodex/DigitalRolodexClassLibrary/IContact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalRolodexClassLibrary {
+    public interface IContact {
+
+        string Name { get; }
+        string Phone { get; }
+        string Email { get; }
+        string Address { get; }
+    }
+}
---
+++ DigitalRolodex/DigitalRolodexTests/ContactTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DigitalRolodexClassLibrary;
+
+namespace DigitalRolodexTests {
+    [TestClass]
+    public class ContactTest {
+
+        Contact contact;
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+         "Phone Number Must Contain Exactly 10 Digits.")]
+        public void PhoneDigitsLengthExceeded() {
+
+            contact = new Contact("", "(647)131-21241", "", "");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+         "Phone Number Must Contain Exactly 10 Digits.")]
+        public void NotEnoughDigitsInPhoneNumber() {
+
+            contact = new Contact("", "(647)131-211", "", "");
+        }
+
+        [TestMethod]
+        public void PhoneNumberFormatted() {
+
+            contact = new Contact("", "*647!13@10211", "", "");
+
+            Assert.AreEqual("(647)131-0211", contact.Phone);
+        }
+    }
+}
