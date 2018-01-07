c92be14f473eb5605799612d4fa1d4e5b699f029
YI ZHANG <scaccterran@gmail.com>
2018-01-07 16:31:06 -0500

    update functionality and corresponding UI

--- DigitalRolodex/DigitalRolodex/MainForm.cs
+++ DigitalRolodex/DigitalRolodex/MainForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DigitalRolodexClassLibrary;
+using DigitalRolodexControlLibrary;
 
 namespace DigitalRolodex {
     public partial class MainForm : Form {
@@ -15,6 +16,7 @@ namespace DigitalRolodex {
         #region Private Fields
         private IContactDataAccess DataAccess { get; set; }
         private RolodexValidator Validator { get; set; }
+        private UpdateContactPanel UpdateContactPanel { get; set; }
         private DataSet Contacts { get; set; }
         private Point MouseXY { get; set; }
         private bool Maximized { get { return WindowState == FormWindowState.Maximized; } }
@@ -23,17 +25,41 @@ namespace DigitalRolodex {
         public MainForm() {
 
             InitializeComponent();
-            NewContactPanel.InjectService(new TextBoxValidator());
             LoadAssets();
             LoadContact();
         }
 
-        private void LoadAssets() {
+        #region Setups and Initializations
+        private void CreateValidators() {
 
             DataAccess = new ContactDataAccess();
             var phoneValidator = new PhoneNumberValidator("areaCode.txt");
             Validator = new RolodexValidator(phoneValidator);
         }
+
+        private void AddUpdateContactPanel() { 
+        
+            UpdateContactPanel = new UpdateContactPanel();
+            UpdateContactPanel.Dock = DockStyle.Fill;
+            UpdateContactPanel.Visible = false;
+            UpdateContactPanel.OnContactAdding += this.UpdateContactPanelOnContactAdding;
+            UserViewPanel.Controls.Add(UpdateContactPanel);
+        }
+
+        private void InjectServices() {
+
+            var service = new TextBoxValidator();
+            NewContactPanel.InjectService(service);
+            UpdateContactPanel.InjectService(service);
+        }
+
+        private void LoadAssets() {
+
+            CreateValidators();
+            AddUpdateContactPanel();
+            InjectServices();
+        }
+        #endregion
 
         private void ShowPanel(UserControl panel) {
 
@@ -41,20 +67,31 @@ namespace DigitalRolodex {
             panel.BringToFront();
         }
 
-        private Contact CreateContact() {
+        private Contact CreateContact(IInputPanel panel) {
 
-            string name = NewContactPanel.Inputs[0];
-            string phone = NewContactPanel.Inputs[1];
-            string email = NewContactPanel.Inputs[2];
-            string address = NewContactPanel.Inputs[3];
+            string name = panel.Inputs[0];
+            string phone = panel.Inputs[1];
+            string email = panel.Inputs[2];
+            string address = panel.Inputs[3];
 
             return new Contact(name, phone, email, address);
         }
 
-        #region CRUD Operations Handler
+        private void LoadUpdateInformation() {
+
+            int id = ViewContactPanel.SelectedID;
+            var toUpdate = Contacts.Tables["Contact"]
+                                   .AsEnumerable()
+                                   .Where(contact => contact.Field<int>("id") == id)
+                                   .First();
+
+            UpdateContactPanel.LoadContact(toUpdate);
+        }
+
+        #region CRUD Operations Handling
         private void AddContact() {
 
-            DataAccess.Insert(CreateContact());
+            DataAccess.Insert(CreateContact(NewContactPanel));
             LoadContact();
         }
 
@@ -66,7 +103,9 @@ namespace DigitalRolodex {
 
         private void UpdateContact() {
 
-            //TODO: not yet implemented
+            var contact = CreateContact(UpdateContactPanel);
+            DataAccess.Update(contact, ViewContactPanel.SelectedID);
+            LoadContact();
         }
 
         private void DeleteContact() {
@@ -103,12 +142,30 @@ namespace DigitalRolodex {
 
         private void ViewContactPanelOnContactUpdating(object sender, EventArgs e) {
 
-            UpdateContact();
+            ShowPanel(UpdateContactPanel);
+            LoadUpdateInformation();
         }
 
         private void ViewContactPanelOnContactDeleting(object sender, EventArgs e) {
 
             DeleteContact();
+        }
+
+        private void UpdateContactPanelOnContactAdding(object sender, EventArgs e) {
+
+            var errors = Validator.FindInputErrors(UpdateContactPanel.Inputs);
+
+            if(errors.Length > 0) {
+
+                UpdateContactPanel.ShowInvalidFields(errors);
+            }
+            else {
+
+                UpdateContact();
+                ViewContactPanel.CollapseEditPanel();
+                UpdateContactPanel.Reset();
+                UpdateContactPanel.Visible = false;
+            }
         }
 
         private void SearchBoxOnSearch(object sender, EventArgs e) {
---
+++ DigitalRolodex/DigitalRolodexControlLibrary/UpdateContactPanel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace DigitalRolodexControlLibrary {
+    public partial class UpdateContactPanel : NewContactPanel {
+
+        public UpdateContactPanel() {
+
+            InitializeComponent();
+            AddCustomStyle();
+            AddCustomHandler();
+        }
+
+        public void LoadContact(DataRow information) {
+
+            NameInputBox.Text = information.Field<string>("Name");
+            PhoneInputBox.Text = information.Field<string>("Phone");
+            EmailInputBox.Text = information.Field<string>("Email");
+            AddressInputBox.Text = information.Field<string>("Address");
+        }
+
+        #region Custom Style and Events Handling
+        private void AddCustomStyle() {
+
+            AddButton.Text = "Update";
+            AddButton.Image = Properties.Resources.save_contact_button;
+            ResetButton.Text = "Cancel";
+            ResetButton.Image = Properties.Resources.delete_contact_button;
+        }
+
+        private void AddCustomHandler() {
+
+            ResetButton.Click += this.CancelButtonClick;
+        }
+        #endregion
+
+        private void CancelButtonClick(object sender, EventArgs e) {
+
+            this.Visible = false;
+        }
+    }
+}
