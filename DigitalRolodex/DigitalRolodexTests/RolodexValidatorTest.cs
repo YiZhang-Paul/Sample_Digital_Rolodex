2995e116192d45548ac628d3503d087645e9abc6
Yi Zhang <scaccterran@gmail.com>
2020-01-27 15:40:54 -0500

    Update RolodexValidatorTest.cs

--- DigitalRolodex/DigitalRolodexTests/RolodexValidatorTest.cs
+++ DigitalRolodex/DigitalRolodexTests/RolodexValidatorTest.cs
@@ -7,6 +7,7 @@ namespace DigitalRolodexTests {
     [TestClass]
     public class RolodexValidatorTest {
 
+
         Mock<IPhoneNumberValidator> phoneNumberValidator;
         RolodexValidator rolodexValidator;
 
